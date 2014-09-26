65c722068198e1c53d91f7e56a4b4452948d7a45
gregory <greg2502@gmail.com>
2014-09-25 23:50:49 -0700

    add server_request_logs api

---
+++ app/controllers/server_request_logs_controller.rb
@@ -0,0 +1,8 @@
+class ServerRequestLogsController < ApplicationController
+  respond_to :json
+  def top_urls
+
+    report = TopUrlsReport.build
+    respond_with report
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Inalytics::Application.routes.draw do
   root to: 'dashboard#server_request_log'
+  resources :server_request_logs, only: [] do
+    collection do
+      get :top_urls
+    end
+  end
 end
